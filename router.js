3a805749b23022c310ce3e7c03e4974c4a240bf6
Nicolas Vannier <nicolas.vannier93@gmail.com>
2012-11-01 13:07:48 +0100

    Modified > Client files architecture

--- router.js
+++ router.js
@@ -2,4 +2,7 @@ exports.routes = function (app) {
 	app.get('/', function(request, response) {
 		response.render('home.jade');
 	});
+	app.get('/game', function(request, response) {
+		response.render('game.jade');
+	});
 }
